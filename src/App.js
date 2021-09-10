51d9a88b1a309419d3eaddf0fbaca99f4c635f17
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-09-10 16:11:47 -0400

    ll

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import {Link, Switch, Route} from "react-router-dom";
 import countries from './countries.json'
 import Home from './components/Home'
 import CountryDetail from './components/CountryDetail'
-=======
-import { Link, Switch, Route } from 'react-router-dom';
-import countries from './countries.json';
-
->>>>>>> d1c0c7e7127c89ab4bf7474c7494491a3a365e25
 function App() {
 	const CountryList = () => {
 		return countries.map((country) => {
@@ -23,7 +17,6 @@ function App() {
 		});
 	};
 
-<<<<<<< HEAD
   const CountryList = () => {
     return countries.map((country) => {
       return <Link key={country.cca3} to={`/country/${country.cca3}`}><li>{country.flag} {country.name.common}</li></Link>
@@ -50,22 +43,6 @@ function App() {
       </Switch>
     </div>
   );
-=======
-	return (
-		<div className="App">
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">WikiCountries</Link>
-					</li>
-				</ul>
-			</nav>
-			<ul id="countrylist">
-				<CountryList />
-			</ul>
-		</div>
-	);
->>>>>>> d1c0c7e7127c89ab4bf7474c7494491a3a365e25
 }
 
 export default App;
