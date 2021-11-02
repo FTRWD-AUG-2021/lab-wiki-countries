5d61c5e6db84459fbb316ca8f4bd89845ca536fb
Michael Nazareth <mcnazareth@gmail.com>
2021-11-02 13:01:51 -0400

    annotating deliverable

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import countries from './countries.json';
 import Home from './components/Home';
 import CountryDetail from './components/CountryDetail';
 function App() {
+  // okay so I want to map out the data available to me. so im going to import that data in the form of countries. i want to map out that data so i will return countries.map((eachCountry)=>{
+
+  // return (<link key=individual-identifier to={/country/}) with a list iof the flag and name.} )
   const CountryList = () => {
+    console.log(countries);
     return countries.map((country) => {
+      // this is our list. we are listing down all the countries. each list item will have its link
+      //  and it will have its flag as an image.
       return (
         <Link key={country.cca3} to={`/country/${country.cca3}`}>
+          {/* each item will link to its cca3 (the abbreviation). remember this is a link that is created for 
+      each mapped out item. so each item then gets a link created for it. when clicked it goes to that countries
+       */}
           <li>
             {country.flag} {country.name.common}
+            {/* here we are mapping our our list of countries. we use countries.map((country)=>{
+              return the country flag and name which is wrapped in a link tag that routes us to the component details.
+            }) */}
           </li>
         </Link>
       );
@@ -23,19 +35,25 @@ function App() {
         <ul>
           <li>
             <Link to="/">WikiCountries</Link>
+            {/* our nav bar contains just one link, which is to our home page. we use a Link to=/ */}
           </li>
         </ul>
       </nav>
       <ul id="countrylist">
+        {/* now we want to create and place our CountryList component. we map out our list above and now we place it here in a unordered list tag */}
         <CountryList />
       </ul>
 
       <Switch>
+        {/* here we create our switch functionality which basically allows us to switch which route is being displayed. this way each time we select a new country, it replaces the old country. */}
         <Route exact path="/" component={Home} />
         <Route
           exact
           path="/country/:cca3OfTheCountry"
+          //this creates a route for each country.
+          // each country route will also have attached to it the countryDetail component, which will display the corresponding data.
           component={CountryDetail}
+          //each time a countries link is clicked, we initiate the CountryDetail component which will match the selected link cca3 to the cca3 details by grabbing it from the json. it will then return the details in the form of headers. each path is the cca3OFTheCountry as found in props.
         />
       </Switch>
     </div>
--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom';
 
 const CountryDetail = (props) => {
   const [theCountry, setTheCountry] = useState({ borders: [] });
-
+  //i Want this componant to display the details of each selected country. I want it to display the country, the capital, the language, the map, the flag, and the bordering coutnries. this means im going to have to map out the data into a div. becuase i want the componant to change based off of the newly selected countries that also means im going to need to use useState and useEffect. usEffect will be used to set the state everytime a country is selected. everytime the country is selected and the new country is set, the page will re-render to reflect the newly mapped out country.
   useEffect(() => {
     let country = countries.find((eachCountry) => {
+      //here we are wanting to display the selected country. so we create a variable known as country. in this variable we want to store and display the information from the selected country. so we import our countiries from our database and we use a find method to search. now our search parameters are going to be at variable to whatever we select, and we dont want to make a hundred links. we are going to be finding a lot of countries so we want to be able to use the data we have to find the data we need. so in our find method we are going to need a function that helps us find the selected data. each country is going to have a unique cca3, which is its abberviated Id. so we want to find that id. in order to do this we need to create a function. the function is basically just going to act as instructions for the computer. we want the computer to take the sele
+      console.log(props);
+
+      console.log(countries[1]);
       return eachCountry.cca3 === props.match.params.cca3OfTheCountry;
+      //we match the selected cca3 to the props(the title for the objects passing into this function.)match.params.cca3ofTheCountry. we do this by console.log(props) the name representing what is being passed into the function (const CountryDetail = (props) =>{ }  )
     });
     console.log(country);
     setTheCountry(country);
@@ -17,6 +22,7 @@ const CountryDetail = (props) => {
     let borders = theCountry.borders.map((border) => {
       return countries.find((country) => {
         return country.cca3 === border;
+        //since the borders are is an array that we want to assin links to we need extract those borders, map them as a list with a link. this is going to be its own component
       });
     });
     return borders.map((border) => {
